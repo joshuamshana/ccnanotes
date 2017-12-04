6908f7546fb0ec7413ecdf1c35312b0eafca7de8
joshua <mama27j@gmail.com>
2017-12-04 15:46:54 +0300

    add webview

--- app/src/main/java/com/fahamutech/cs243networkdesign/MainActivity.java
+++ app/src/main/java/com/fahamutech/cs243networkdesign/MainActivity.java
@@ -23,7 +23,7 @@ public class MainActivity extends AppCompatActivity
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        getActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
