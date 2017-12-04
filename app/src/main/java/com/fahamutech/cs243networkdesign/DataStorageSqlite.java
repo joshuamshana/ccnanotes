3ac50ac734b753f5282ff7cb357ec0fa4d3be9a5
joshua <mama27j@gmail.com>
2017-12-05 00:21:27 +0300

    add database class

---
+++ app/src/main/java/com/fahamutech/cs243networkdesign/DataStorageSqlite.java
@@ -0,0 +1,113 @@
+package com.fahamutech.cs243networkdesign;
+
+/**
+ * Created by joshua , 12/4/17.
+ */
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.DatabaseUtils;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class DataStorageSqlite extends SQLiteOpenHelper {
+
+    public static final String DATABASE_NAME = "NetworkDesignNotes.db";
+    public static final String CONTACTS_TABLE_NAME = "network";
+    public static final String CONTACTS_COLUMN_ID = "id";
+    public static final String CONTACTS_COLUMN_UNIT = "unit";
+    public static final String CONTACTS_COLUMN_NOTES = "notes";
+
+    public DataStorageSqlite(Context context) {
+        super(context, DATABASE_NAME , null, 1);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        // TODO Auto-generated method stub
+        db.execSQL(
+                "create table network " +
+                        "(id integer primary key autoincrement, " +
+                        "unit text,notes blob)"
+        );
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        // TODO Auto-generated method stub
+        db.execSQL("DROP TABLE IF EXISTS contacts");
+        onCreate(db);
+    }
+
+    public boolean insertContact (String name,
+                                  String phone,
+                                  String email,
+                                  String street,
+                                  String place) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put("name", name);
+        contentValues.put("phone", phone);
+        contentValues.put("email", email);
+        contentValues.put("street", street);
+        contentValues.put("place", place);
+        db.insert("contacts", null, contentValues);
+        return true;
+    }
+
+    public Cursor getData(int id) {
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor res =  db.rawQuery( "select * from contacts where id="+id+"", null );
+        return res;
+    }
+
+    public int numberOfRows(){
+        SQLiteDatabase db = this.getReadableDatabase();
+        int numRows = (int) DatabaseUtils.queryNumEntries(db, CONTACTS_TABLE_NAME);
+        return numRows;
+    }
+
+    public boolean updateContact (Integer id,
+                                  String name,
+                                  String phone,
+                                  String email,
+                                  String street,
+                                  String place) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put("name", name);
+        contentValues.put("phone", phone);
+        contentValues.put("email", email);
+        contentValues.put("street", street);
+        contentValues.put("place", place);
+        db.update("contacts",
+                contentValues, "id = ? ", new String[] { Integer.toString(id) } );
+        return true;
+    }
+
+    public Integer deleteContact (Integer id) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        return db.delete("contacts",
+                "id = ? ",
+                new String[] { Integer.toString(id) });
+    }
+
+    public ArrayList<String> getAllCotacts() {
+        ArrayList<String> array_list = new ArrayList<String>();
+
+        //hp = new HashMap();
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor res =  db.rawQuery( "select * from contacts", null );
+        res.moveToFirst();
+
+        while(res.isAfterLast() == false){
+            //array_list.add(res.getString(res.getColumnIndex(CONTACTS_COLUMN_NAME)));
+            res.moveToNext();
+        }
+        return array_list;
+    }
+}
